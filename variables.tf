90e71dca15870affb18144d403796c350b916f49
Vladislav Matetic <vmatetic@hotmail.com>
2024-03-21 11:38:45 +0000

    version v1.0.0

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "storage_account_name" { 
+    type = string
+}
+
+variable "name" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
